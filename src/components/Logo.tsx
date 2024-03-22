d41aa05d37efe6d807d116ed0b56019385a81808
Chad Christensen <chadchristensen2@gmail.com>
2024-03-22 13:00:59 -0400

    [story: 2] update logo in the header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
-import { Speaker } from "lucide-react";
+import { CONTENT } from "@/lib/constants";
+import { Code2 } from "lucide-react";
 import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link href="/" data-test="logo"><Speaker /><span className="sr-only">Hip Hop Shop</span></Link>
+        <Link href="/" data-test="logo"><Code2 /><span className="sr-only">{CONTENT.SITE_NAME}</span></Link>
     )
 }
