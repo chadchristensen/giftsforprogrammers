a674829e01f7a5c633d44684bb8fd23c71c886a5
Chad Christensen <chadchristensen2@gmail.com>
2024-03-27 11:03:00 -0400

    [story: 22] update config for remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
                 hostname: 'loremflickr.com',
                 port: '',
             },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/dswjpamey/**'
+            }
         ],
     },
 };
