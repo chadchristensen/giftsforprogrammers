4d33973458d46ea4d26009ef2d12fe36147c350a
Chad Christensen <chadchristensen2@gmail.com>
2024-04-09 20:42:54 -0400

    [story: 56] update div to semantic footer tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
-        <div className="w-full bg-primary py-8 text-slate-50">
+        <footer className="w-full bg-primary py-8 text-slate-50">
             <div className="container mx-auto md:px-40 text-center">
                 Made with ❤️ and <img className="inline-block" width="22px" src="/code_icon.svg" alt="code icon" /> by <a className="underline text-blue-200" target="_blank" rel="noopener" href="https://www.linkedin.com/in/chadthecoder/">chadthecoder</a>
             </div>
-        </div>
+        </footer>
     )
 }
